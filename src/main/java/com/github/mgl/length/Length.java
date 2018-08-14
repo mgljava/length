8b505f77d80ee85410adc1838618dfe783d70dce
SweepMonk <guangliang.ming@baozun.com>
2018-08-14 16:21:49 +0800

    [LENGTH][明光亮] 修改代码

--- src/main/java/com/github/mgl/length/Length.java
+++ src/main/java/com/github/mgl/length/Length.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * 长度实体
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/github/mgl/length/LengthUnit.java
+++ src/main/java/com/github/mgl/length/LengthUnit.java
@@ -1,8 +1,5 @@
 package com.github.mgl.length;
 
-/**
- * 长度单位
- */
 public enum LengthUnit {
   MILE {
     private static final int MILE_TO_BASE_UNIT = 1760 * 12 * 3;
